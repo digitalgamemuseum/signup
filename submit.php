e8c3676e4133cbc60b8670648719afa56fd55dfa
Digital Game Museum IT <dgm-it@digitalgamemuseum.org>
2015-07-08 10:34:29 -0700

    add visitor/volunteer radio buttons

--- submit.php
+++ submit.php
@@ -13,6 +13,8 @@ if($_POST)
     
     $user_email = $_POST["user_email"];
     $user_comments = $_POST["user_comments"];
+    $volunteer = $_POST["volunteer"];
+    $volunteer_name = $_POST["volunteer_name"];
     $btdt = ($_POST["btdt"] === "true");
     
     $dbConnection = new PDO('mysql:dbname=dgm;host=localhost;charset=utf8', 'dgm', 'dgm');
@@ -20,9 +22,9 @@ if($_POST)
     $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $dbConnection->prepare('INSERT INTO data (email, comments, btdt, timestamp) VALUES (:email, :comments, :btdt, NOW())');
+    $stmt = $dbConnection->prepare('INSERT INTO data (email, comments, volunteer, volunteer_name, btdt, timestamp) VALUES (:email, :comments, :volunteer, :volunteer_name, :btdt, NOW())');
 
-    $stmt->execute(array('email' => $user_email, 'comments' => $user_comments, 'btdt' => $btdt));
+    $stmt->execute(array('email' => $user_email, 'comments' => $user_comments, 'volunteer' => $volunteer, 'volunteer_name' => $volunteer_name, 'btdt' => $btdt));
     
     die(json_encode(array('type'=>'done')));
 }
